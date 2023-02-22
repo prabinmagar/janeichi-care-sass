e521adb20c1293620ce5f15a95c5e936e248df84
Prabin Magar <psmagar123451@gmail.com>
2023-02-22 22:39:23 +0545

    source code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,8 +31,8 @@ jQuery(function(){
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 2,
+              slidesToScroll: 2,
               infinite: true,
               dots: true
             }
@@ -40,17 +40,103 @@ jQuery(function(){
           {
             breakpoint: 768,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 576,
-            settings: {
               slidesToShow: 1,
               slidesToScroll: 1
             }
           }
         ]
-      });
+    });
+
+    $('.feedback-slider').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true
+    });
+
+    $('.card-box-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+          }
+        },
+        {
+          dots: true,
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+    $('.insta-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 4,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+    });
+
+    $('.review-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+      ]
+    });
 });
